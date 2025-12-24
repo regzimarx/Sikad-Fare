624c3d193b81fe8d5e5a81167ab82dab833408b5
User <vargas.mattykun@gmail.com>
2025-12-24 18:52:36 +0800

    0._.2 PWA Functionality for Offline Use

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,10 +2,13 @@ import React from 'react';
 import NavigationWrapper from '@/components/NavigationWrapper'; 
 import SupportButton from '@/components/SupportButton';
 import './globals.css';
+import { Toaster } from 'react-hot-toast';
+import PwaInstallPrompt from '@/components/PwaInstallPrompt';
 
 export const metadata = {
   title: 'Sikad Fare Calculator', 
   description: 'Fare Guidance for Sikad Drivers and Passengers in Midsayap, Cotabato',
+  manifest: '/manifest.json',
 };
 
 export default function RootLayout({
@@ -22,6 +25,12 @@ export default function RootLayout({
           </NavigationWrapper>
         </main>
         <SupportButton />
+        <PwaInstallPrompt />
+        <Toaster 
+          containerStyle={{
+            zIndex: 99999, // Force it to be above everything else
+          }}
+        />
       </body>
     </html>
   );
